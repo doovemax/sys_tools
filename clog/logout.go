de9091aba7fce314f9e8ce0e44d7eca7e4197346
doovemax <445001843@qq.com>
2018-10-08 18:01:47 +0800

    feat: console 输出

--- clog/logout.go
+++ clog/logout.go
@@ -6,6 +6,24 @@ import (
 	"time"
 )
 
+const (
+	L_BLACK  = `\e[1;30m`
+	RED      = `\e[0;31m`
+	L_RED    = `\e[1;31m`
+	GREEN    = `\e[0;32m`
+	L_GREEN  = `\e[1;32m`
+	BROWN    = `\e[0;33m`
+	YELLOW   = `\e[1;33m`
+	BLUE     = `\e[0;34m`
+	L_BLUE   = `\e[1;34m`
+	PURPLE   = `\e[0;35m`
+	L_PURPLE = `\e[1;35m`
+	CYAN     = `\e[0;36m`
+	L_CYAN   = `\e[1;36m`
+	GRAY     = `\e[0;37m`
+	WHITE    = `\e[1;37m`
+)
+
 var (
 	timeZone *time.Location
 	err      error
